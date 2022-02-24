da2e100d2f0eef2b83ff691e490a94ef3c58167c
Matveev Timofey <justshowel@gmail.com>
2022-02-24 17:26:47 +0300

    feat: Implemented function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let num = 0;
+  while (n > 0)
+  {
+      num *= 10;
+      num += n % 10;
+      n = Math.floor(n / 10);
+  }
+  return num;
 }
